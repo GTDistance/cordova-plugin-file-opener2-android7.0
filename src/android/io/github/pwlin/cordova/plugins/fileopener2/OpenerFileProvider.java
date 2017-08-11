f066e88b6b25cc9d9b217b81e60da66f6cdd7421
GTDistance <zdhwanglc@163.com>
2017-08-11 11:13:46 +0800

    修改和camera的provider冲突的问题

---
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/OpenerFileProvider.java
@@ -0,0 +1,9 @@
+package io.github.pwlin.cordova.plugins.fileopener2;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Thomas.Wang on 2017/8/11.
+ */
+public class OpenerFileProvider extends FileProvider {
+}
